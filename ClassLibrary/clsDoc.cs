c98d2d5084b0b32bec3df8daef645cd3281dcf77
Purple Rabbit <131457545+J-Purple-Rabit@users.noreply.github.com>
2026-05-05 09:54:10 +0100

    creeated fields in doc.cls answell as the test

--- ClassLibrary/clsDoc.cs
+++ ClassLibrary/clsDoc.cs
@@ -2,5 +2,11 @@
 {
     public class clsDoc
     {
+        public bool DAvailability { get; set; }
+        public string DPhoneNumber { get; set; }
+        public string DEmail { get; set; }
+        public string DAddress { get; set; }
+        public string DLastName { get; set; }
+        public string DFirstName { get; set; }
     }
 }
